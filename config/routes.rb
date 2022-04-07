f0d7d63c399ca09b239d99e6baaa78a8d9a837df
yayoi <luftpause@gmail.com>
2022-04-07 02:40:32 +0000

    controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
-
-
   resources :contents
-
-
-  
-  devise_for :users, controllers: { registrations: 'users/registrations' }
-  
   root to: "home#index"
-  get 'users/show' => 'users#show' 
+  
+  
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords',
+    :confirmations => 'users/confirmations',
+    :unlocks => 'users/unlocks'
+    }
+  devise_scope :user do
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    delete "logout", :to => "users/sessions#destroy"
+    get 'users/show', :to => 'users#show' 
+  end
+  
 end
