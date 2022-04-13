40dfffdfd460818985a114e9f9462ff28863f135
yayoioz <luftpause@gmail.com>
2022-04-13 10:41:49 +0000

    relationship model recreate

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,30 +1,26 @@
 class RelationshipsController < ApplicationController
- before_action :set_user
+ 
 
   def create
-    following = current_user.follow(@user)
-    if following.save
-      flash[:success] = "ユーザーをフォローしました"
-      redirect_to @user
-    else
-      flash.now[:alert] = "ユーザーのフォローに失敗しました"
-      redirect_to @user
-    end
+    current_user.follow(params[:user_id])
+    redirect_to request.referer
   end
 
   def destroy
-    following = current_user.unfollow(@user)
-    if following.destroy
-      flash[:success] = "ユーザーのフォローを解除しました"
-      redirect_to @user
-    else
-      flash.now[:alert] = "ユーザーのフォロー解除に失敗しました"
-      redirect_to @user
-    end
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer
+  end
+  
+  # フォロー一覧
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+  # フォロワー一覧
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
   end
 
-  private
-  def set_user
-    @user = User.find(params[:relationship][:follow_id])
-  end
+
 end
--- db/migrate/20220412012039_create_relationships.rb
+++ db/migrate/20220412012039_create_relationships.rb
@@ -1,12 +1,11 @@
 class CreateRelationships < ActiveRecord::Migration[5.2]
   def change
     create_table :relationships do |t|
-      t.references :user, foreign_key: true
-      t.references :follow, foreign_key: { to_table: :users }
+      t.integer :follow_id, foreign_key: true
+      t.integer :target_id, foreign_key: true
 
       t.timestamps
       
-      t.index [:user_id, :follow_id], unique: true
     end
   end
 end
