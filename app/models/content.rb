211b22205d9492e3c66c221a050845b66454b4ea
yayoioz <luftpause@gmail.com>
2022-05-02 09:54:09 +0000

    content rb update

--- app/models/content.rb
+++ app/models/content.rb
@@ -19,6 +19,18 @@ class Content < ApplicationRecord
     tag_list = tags.splot(/[[:blank:]]+/)
     
     current_tags = self.tags.pluck(:name)
+    
+    #　1）元々自分に紐付いていたタグと投稿されたタグの差分を抽出
+    #　　-- 記事更新時に削除されたタグ
+    old_tags =current_tags - tag_list
+    
+    #　2）投稿されたタグと元々自分に紐付いていたタグの差分を抽出
+    # 　　-- 新規に追加されたタグ
+    new_tags = tag_list - current_tags
+    p current_tags
+    
+    #tag_content から　1を削除
+    
   end
 
 end
